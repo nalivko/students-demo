4482a94a2f3cb906d1ca783d6da156a9dba544e0
Dmitriy <nalivko.dmitr@gmail.com>
2024-05-10 11:11:37 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+import express, {Request, Response} from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  let message = 'Hello Dimka!!!'
+  res.send(message)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
